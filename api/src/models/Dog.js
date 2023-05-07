6a29fc51289f32b3f3c09b8140fedf79d8727874
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-05-07 01:17:33 -0300

    empezando back

---
+++ api/src/models/Dog.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID, //único.
+        allowNull: false, //campo requerido, no puede estar vacío.
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      life_span: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createBD: {
+        //Todo lo que yo cree se crea con esta propiedad, es más eficiente.
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    },
+    { timestamps: true }
+  );
+};

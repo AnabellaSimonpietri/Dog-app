37cacdf7c55a8829b8d90effb8ecc2e029c5b464
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-05-26 05:12:01 -0300

    back ok

--- api/src/models/Temperaments.js
+++ api/src/models/Temperaments.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+// Exporto una funcion que define el modelo
+// Luego la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define(
